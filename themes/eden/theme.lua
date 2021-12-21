9d96aa6d98ec8f8de888bed7e522c5925ae0235c
Anna Danilovna <annadanilovna@protonmail.com>
2021-12-21 08:29:45 -0800

    fix spacing

--- themes/eden/theme.lua
+++ themes/eden/theme.lua
@@ -18,7 +18,7 @@ local my_table = awful.util.table or gears.table -- 4.{0,1} compatibility
 
 local theme                                     = {}
 theme.dir                                       = os.getenv("HOME") .. "/.config/awesome/themes/eden"
-theme.wallpaper                         = theme.dir .. "/wall.png"
+theme.wallpaper                                 = theme.dir .. "/wall.png"
 theme.fnit                                      = "IBM Plex Mono Text 8"
 theme.fg_normal                                 = "#DDDDFF"
 theme.fg_focus                                  = "#EA6F81"
